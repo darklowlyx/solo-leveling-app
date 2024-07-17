3a981319dcf2a63c759e4c1c86d1b219089085ee
darklowlyx <dark.codx.lowly@gmail.com>
2024-07-17 06:49:58 +0500

    0.3.5

--- js/script.js
+++ js/script.js
@@ -15,16 +15,16 @@ document.addEventListener("DOMContentLoaded", () => {
 					flash.classList.add("show");
 
 					setTimeout(() => {
-							initialScreen.style.display = "none"; // Hide initial screen
-							mainContent.classList.add("active");  // Show main content
+							initialScreen.style.display = "none";
+							mainContent.classList.add("active");
 							flash.classList.remove("show");
 
 							setTimeout(() => {
 									document.body.removeChild(flash);
 							}, 500);
 
-							backgroundMusic.play();               // Play background music
-					}, 500); // Wait for the button to expand
-			}, 500); // Wait for the button to expand
+							backgroundMusic.play();
+					}, 500);
+			}, 500); 
 	});
 });
