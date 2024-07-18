f89737f130896c1f589bac5f28cfd541dbc7abde
darklowlyx <dark.codx.lowly@gmail.com>
2024-07-18 19:38:41 +0500

    0.4

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const initialScreen = document.querySelector(".initial");
 	const startButton = document.querySelector(".initial__button");
+	const muteButton = document.querySelector(".initital__mute");
+	const muteSpan = muteButton.querySelector("span");
 	const mainContent = document.querySelector(".page__main");
-	const transitionSound = document.getElementById("transition-sound")
+	const transitionSound = document.getElementById("transition-sound");
+	const introSong = document.getElementById("intro-song");
+	const backgroundSong = document.getElementById("background-song");
 
 	startButton.addEventListener("click", () => {
 			startButton.classList.add("fullscreen");
@@ -12,16 +16,27 @@ document.addEventListener("DOMContentLoaded", () => {
 					flash.className = "flash";
 					document.body.appendChild(flash);
 					flash.classList.add("show");
-
+					introSong.pause();
 					setTimeout(() => {
 							initialScreen.style.display = "none";
 							mainContent.classList.add("active");
 							flash.classList.remove("show");
-
+							backgroundSong.play();
 							setTimeout(() => {
 									document.body.removeChild(flash);
 							}, 500);
 					}, 500);
 			}, 500); 
+	});
+
+	muteButton.addEventListener("click", () => {
+		if (!muteSpan.classList.contains("mute-t")) {
+      muteSpan.classList.add("mute-t");
+			introSong.play();
+			introSong.muted = false; 
+    } else {
+      muteSpan.classList.remove("mute-t");
+			introSong.muted = true;
+    }
 	});
 });
